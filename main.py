c9d876f650a0b39c247983bd3a6f3cb6a6a6556c
Jenifer05a <mari109mtz@gmail.com>
2026-03-18 08:01:37 -0600

    Actualizacion

--- main.py
+++ main.py
@@ -4,23 +4,18 @@ from datetime import datetime
 
 app = FastAPI()
 
-# 🔹 Pega aquí tu conexión de MongoDB
-MONGO_URI = "mongodb+srv://<db_username>:<db_password>@cluster0.vzr9qw6.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://esp32:esp32pass@cluster0.vzr9qw6.mongodb.net/iot?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_URI)
 db = client.iot
 collection = db.sensores
 
-# Ruta de prueba
 @app.get("/")
 def root():
     return {"mensaje": "API funcionando"}
 
-# Ruta para guardar datos
 @app.post("/sensor")
 def guardar_sensor(data: dict):
     data["fecha"] = datetime.now()
     collection.insert_one(data)
     return {"status": "dato guardado"}
-
-
